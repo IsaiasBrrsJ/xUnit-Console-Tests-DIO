d4a32ddab24bfa78a6b2d52c2d3176a559adedbc
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2023-10-17 19:39:28 -0400

    Projetos tests com xUnit dio.io

---
+++ Model/OperacoesCalculadora.cs
@@ -0,0 +1,66 @@
+﻿using Calculadora.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Operacoes.Calculadora
+{
+    public class OperacoesCalculadora
+    {
+        private readonly  List<string>  historicoResultado;
+
+       
+        public OperacoesCalculadora()
+        {
+            historicoResultado = new List<string>();
+        }
+
+
+        public List<string> RetornaLista()
+        {
+            return historicoResultado;
+        }
+
+        public int Somar(int x, int y)
+        {   
+            var resultado =  x + y;
+            historicoResultado.Add( Util.ConverteParaString(resultado));
+
+            return resultado;
+        }
+
+        public int Multiplicar(int x, int y)
+        {
+            var resultado = x * y;
+            historicoResultado.Add( Util.ConverteParaString(resultado));
+
+            return resultado;
+        }
+
+        public int Dividir(int x, int y)
+        {
+            var resultado = x / y;
+
+            if(resultado > 0)
+            {
+                historicoResultado.Add(Util.ConverteParaString(resultado));
+            }
+         
+
+            return resultado;
+        }
+
+        public int Subtrair(int x, int y)
+        {
+            var resultado = x - y;
+            historicoResultado.Add(Util.ConverteParaString(resultado));
+
+            return resultado;
+        }
+
+
+
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Util
+{
+    public static class Util
+    {
+        public static string ConverteParaString(Object valor)
+        {
+            return valor.ToString();
+        }
+    }
+}
